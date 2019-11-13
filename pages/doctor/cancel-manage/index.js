bcc0d0ec60114d8d7a0a4130129bb172be63394e
eileen0817 <1322749014@qq.com>
2019-11-13 09:02:56 +0800

    新增列表

---
+++ pages/doctor/cancel-manage/index.js
@@ -0,0 +1,49 @@
+
+Page({
+  data: {
+    listData: {
+      data: [
+        {
+          name: '姓名',
+          value: '袁铭轩'
+        },
+        {
+          name: '性别',
+          value: '男'
+        },
+        {
+          name: '出生日期',
+          value: '2019-06-30'
+        },
+        {
+          name: '体检',
+          value: '3月龄'
+        },
+        {
+          name: '体检日期',
+          value: '2019-09-30'
+        },
+        {
+          name: '逾期',
+          value: '14天'
+        },
+        {
+          name: '父亲',
+          value: '袁一明  15300001699',
+          state:{
+            value:0
+          }
+        } 
+        ,
+        {
+          name: '父亲',
+          value: '爱美丽  15300001699',
+          state:{
+            value:1
+          }
+        } 
+      ]
+    },
+  },
+  
+})
