83f368a8cdba8448bf519a414fe41608415a1921
eileen0817 <1322749014@qq.com>
2019-11-13 09:55:06 +0800

    提取列表公共模板

--- pages/doctor/cancel-manage/index.js
+++ pages/doctor/cancel-manage/index.js
@@ -1,8 +1,11 @@
-
+import custlist from '/components/custlist';
 Page({
+  ...custlist,
   data: {
     listData: {
-      data: [
+      dingTap: 'handleDingItemTap',
+      btnTap: 'handleBtnTapTap',
+      list: [
         {
           name: '姓名',
           value: '袁铭轩'
@@ -29,21 +32,40 @@ Page({
         },
         {
           name: '父亲',
-          value: '袁一明  15300001699',
-          state:{
-            value:0
+          value: '袁一明',
+          extraText:'15300001699',
+          state: {
+            value: 0
           }
-        } 
+        }
         ,
         {
           name: '父亲',
-          value: '爱美丽  15300001699',
-          state:{
-            value:1
+          value: '爱美丽',
+          extraText:'15300001699',
+          state: {
+            value: 1
           }
-        } 
-      ]
+        }
+      ],
+      btnText: '取消儿童管理'
     },
   },
-  
+  handleBtnTapTap(e) {
+    dd.showToast({
+      content: `55555`,
+      success: (res) => {
+
+      },
+    });
+  },
+  handleDingItemTap(e) {
+    dd.showToast({
+      content: `${e.currentTarget.dataset.value}`,
+      success: (res) => {
+
+      },
+    });
+  }
+
 })
--- pages/doctor/index/index.js
+++ pages/doctor/index/index.js
@@ -1,25 +1,8 @@
-import list from '/components/list';
-
+ 
 Page({
-  ...list,
+ 
   data: {
-    listData: {
-      onItemTap: 'handleListItemTap',
-      header: 'list1',
-      data: [
-        {
-          title: '标题文字',
-          extra: '基本使用'
-        }
-      ]
-    },
+    
   },
-  handleListItemTap(e) {
-    dd.showToast({
-      content: `第${e.currentTarget.dataset.index}个Item`,
-      success: (res) => {
-
-      },
-    });
-  }
+   
 })
