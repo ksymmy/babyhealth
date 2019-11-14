02c387664ca53710e765b0ef230f57a4ce377867
lingyingtan <1920690890>
2019-11-14 14:18:56 +0800

    添加医生端首页前段调用接

--- pages/doctor/index/index.js
+++ pages/doctor/index/index.js
@@ -1,18 +1,41 @@
-
+import {HTTP} from '/util/http.js';
+let http = new HTTP();
+var app = getApp()
 Page({
-
   data: {
-    overTime: {
-      time1: 26,
-      time2: 12,
-      time3: 6
-    },
-    personNum: {
-      notifyNum: 12,
-      changeNum: 26,
-      total: 1026
-    }
+    // overTime: {
+    //   time1: 26,
+    //   time2: 12,
+    //   time3: 6
+    // },
+    // personNum: {
+    //   notifyNum: 12,
+    //   changeNum: 26,
+    //   total: 1026
+    // }
   },
+  onLoad(){
+    var that = this;
+    http.request({
+      url:"/baby/indexCount",
+      success:function(res){
+        console.log(res)
+        that.setData({
+          overTime: {
+            time1: res.overdueDays7,
+            time2: res.overdueDays14,
+            time3: res.overdueDays21
+          },
+          personNum: {
+            notifyNum: res.tomorrowExaminationBabys,
+            changeNum: res.changeDateBabys,
+            total: res.allBabys
+          }
+        })
+      }
+    })
+  },
+  
   // 跳转页面
   viewList(e) {
     dd.navigateTo({
