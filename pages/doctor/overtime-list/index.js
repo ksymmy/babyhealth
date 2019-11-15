20b3403b3133a6b5ab6f2570a3fbe78e42edd05d
eileen0817 <1322749014@qq.com>
2019-11-15 09:12:50 +0800

    新增逾期列表页面及tab+筛选组件

--- pages/doctor/overtime-list/index.js
+++ pages/doctor/overtime-list/index.js
@@ -2,6 +2,7 @@ import msglist from '/util/msglist';
 Page({
   ...msglist,
   data: {
+    //展示数据，请求时更改
     listData: {
       list: [{
         name: '任慕瑶',
@@ -38,38 +39,115 @@ Page({
         textTime: '2019-10-04',
         overTime: 14,
         dingNum: 0
+      },
+      {
+        name: '袁明轩',
+        sex: 1,
+        birthday: '2019-07-04',
+        age: '3月龄',
+        textTime: '2019-10-04',
+        overTime: 14,
+        dingNum: 0
+      },
+      {
+        name: '袁明轩',
+        sex: 1,
+        birthday: '2019-07-04',
+        age: '3月龄',
+        textTime: '2019-10-04',
+        overTime: 14,
+        dingNum: 0
+      },
+      {
+        name: '袁明轩',
+        sex: 1,
+        birthday: '2019-07-04',
+        age: '3月龄',
+        textTime: '2019-10-04',
+        overTime: 14,
+        dingNum: 0
+      },
+      {
+        name: '袁明轩',
+        sex: 1,
+        birthday: '2019-07-04',
+        age: '3月龄',
+        textTime: '2019-10-04',
+        overTime: 14,
+        dingNum: 0
       }],
       type: 3 //封装列表页面展示类型，1为明日体检列表，2为改期列表，3为逾期列表
     },
+    //tab选项内容，badgeText为数字，没有请无需加badgeType，badgeText
     tabs: [
       {
         title: '全部',
         badgeType: 'text',
+        badgeText: '22',
+      },
+      {
+        title: '满月',
+        badgeType: 'text',
+        badgeText: '5',
+      },
+      {
+        title: '3月',
+        badgeType: 'text',
         badgeText: '6',
       },
       {
-        title: '选项二',
-        badgeType: 'dot',
+        title: '6月',
+        badgeType: 'text',
+        badgeText: '3',
       },
-      { title: '3 Tab' },
-      { title: '4 Tab' },
-      { title: '5 Tab' },
+      { title: '8月' },
+      { title: '12月' },
+      { title: '18月' },
+      { title: '24月' },
+      { title: '30月' },
+      { title: '36月' }
     ],
-    activeTab: 2,
+    popList: [{ title: 'DING 0 次', value: 0 }, { title: 'DING 1 次', value: 1 }, { title: 'DING 2 次', value: 2 }, { title: '3次及以上', value: 3 },],//筛选条件
+    activeTab: 2,//当前选中tab序号
+    position: 'bottomRight',//弹出方向
+    popshow: false,//弹出
+    showMask: true,//遮罩层
   },
-   handleTabClick({ index }) {
+  //tab选项切换
+  handleTabClick({ index }) {
+    console.log(index)
     this.setData({
       activeTab: index,
     });
   },
+  //tab选项切换
   handleTabChange({ index }) {
     this.setData({
       activeTab: index,
     });
   },
+  //筛选点击
   handlePlusClick() {
-    my.alert({
-      content: 'plus clicked',
+    this.setData({
+      popshow: !this.data.popshow,
     });
   },
+
+  //遮罩层关闭
+  onMaskClick() {
+    this.setData({
+      popshow: false,
+    });
+  },
+  //筛选条件点击
+  onPopClick({ chooseNum }) {
+    this.setData({
+      popshow: false,
+    });
+    console.log(chooseNum)
+  },
+  //全部ding
+  allDing() {
+    console.log("111")
+  }
 })
