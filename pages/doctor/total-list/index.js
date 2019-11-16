8ae72c2c92698c620e4940b5220ca92816b45840
chengwangkai <cwk123456>
2019-11-16 13:13:31 +0800

    医生端-总管理人数列表数据展示

--- pages/doctor/total-list/index.js
+++ pages/doctor/total-list/index.js
@@ -1,45 +1,23 @@
 import msglist from '/util/msglist';
+import { HTTP } from '/util/http.js';
+import { config } from '/app.js';
+let http = new HTTP(), page = 1;
 Page({
   ...msglist,
   data: {
     listData: {
-      list: [{
-        name: '任慕瑶',
-        sex: 0,
-        birthday: '2019-06-30',
-        age: '3月龄',
-        textTime: '2019-09-30',
-        overTime: 14,
-        dingNum: 3
-      },
-      {
-        name: '任慕瑶',
-        sex: 0,
-        birthday: '2019-04-04',
-        age: '6月龄',
-        textTime: '2019-10-04',
-        overTime: 14,
-        dingNum: 2
-      },
-      {
-        name: '袁明轩',
-        sex: 1,
-        birthday: '2019-07-04',
-        age: '3月龄',
-        textTime: '2019-10-04',
-        overTime: 14,
-        dingNum: 1
-      },
-      {
-        name: '袁明轩',
-        sex: 1,
-        birthday: '2019-07-04',
-        age: '3月龄',
-        textTime: '2019-10-04',
-        overTime: 14,
-        dingNum: 0
-      }],
-      type: 3 //封装列表页面展示类型，1为明日体检列表，2为改期列表，3为逾期列表
+      list: [
+      //   {
+      //   name: '任慕瑶',
+      //   sex: 0,
+      //   birthday: '2019-06-30',
+      //   age: '3月龄',
+      //   textTime: '2019-09-30',
+      //   overTime: 14,
+      //   dingNum: 3
+      // }
+      ],
+      // type: 3 //封装列表页面展示类型，1为明日体检列表，2为改期列表，3为逾期列表
     },
     toptabs: [
       {
@@ -100,6 +78,9 @@ Page({
     ],
     topActiveTab: 0,
     activeTab: [0, 0],
+    inputValue: '',
+    age: '',
+    year: ''
   },
   onLoad() {
     /*获取当前年份最近四年*/
@@ -115,16 +96,71 @@ Page({
       [newTabs]: tabArr,
     });
   },
+  onShow() {
+    page = 1;
+    this.setData({
+      duesrecs: []
+    })
+    this.onRequest();
+  },
+  onSearch: function() {
+    page = 1;
+    this.setData({
+      ['listData.list']: [],
+    });
+    this.onRequest();
+  },
+  onRequest() {
+    let that = this;
+    http.request({
+      url: "baby/allbabyslist",
+      method: 'post',
+      data: JSON.stringify({
+        param: {
+          name: that.data.inputValue,
+          age: that.data.age,
+          year: that.data.year,
+        },
+        page: page,
+        size: config.pageSize,
+      }),
+      success: (res) => {
+        if (res.length == 0) {
+          return
+        }
+        page++;
+        var data = that.data.listData.list;
+        data = data.concat(res);
+        var listIndex = 'listData.list';
+        that.setData({
+          [listIndex]: data,
+        });
+
+        console.log(that)
+
+      },
+      fail: function(res) {
+        dd.alert({ content: JSON.stringify(res), buttonText: '好的' });
+      }
+    })
+  },
+  bindKeyInput(e) {
+    this.setData({
+      inputValue: e.detail.value,
+    });
+  },
   //上方tab选项切换
   handleTopTabClick(index, value) {
     this.setData({
       topActiveTab: index,
+      [index ? 'age' : 'year']: ''
     });
   },
   //上方tab选项切换
   handleTopTabChange(index, value) {
     this.setData({
       topActiveTab: index,
+      [index ? 'age' : 'year']: ''
     });
   },
   //下方tab选项切换
@@ -134,6 +170,18 @@ Page({
       [newIndex]: index,
     });
     console.log(value)
+    if (!isNaN(value.title)) {//年份
+      this.setData({
+        ['age']: '',
+        ['year']: value.value
+      })
+    } else {
+      this.setData({
+        ['age']: value.value,
+        ['year']: ''
+      })
+    }
+    this.onSearch();
   },
   //下方tab选项切换
   handleTabChange(index, value) {
@@ -142,5 +190,17 @@ Page({
       [newIndex]: index,
     });
     console.log(value)
+    if (!isNaN(value.title)) {//年份
+      this.setData({
+        ['age']: '',
+        ['year']: value.value
+      })
+    } else {
+      this.setData({
+        ['age']: value.value,
+        ['year']: ''
+      })
+    }
+    this.onSearch();
   },
 })
