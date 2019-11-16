b62b30bec4f1e6a39e217dbd78577aedb6236a8d
chengwangkai <cwk123456>
2019-11-16 14:48:47 +0800

    医生端-申请改期列表数据展示

--- pages/doctor/change-list/index.js
+++ pages/doctor/change-list/index.js
@@ -1,46 +1,57 @@
 import msglist from '/util/msglist';
+import { HTTP } from '/util/http.js';
+import { config } from '/app.js';
+let http = new HTTP(), page = 1;
 Page({
   ...msglist,
   data: {
     listData: {
-      list: [{
-        name: '任慕瑶',
-        sex: 0,
-        birthday: '2019-04-04',
-        age: '6月龄',
-        textTime: '2019-10-04',
-        changeTime: '2019-10-09',
-        result: '我家宝宝不在社区，我们下周来进行体检1。'
-      },
-      {
-        name: '袁明轩',
-        sex: 1,
-        birthday: '2019-06-30',
-        age: '3月龄',
-        textTime: '2019-09-30',
-        changeTime: '2019-10-09',
-        result: '我家宝宝不在社区，我们下周来进行体检2。'
-      },
-      {
-        name: '任慕瑶',
-        sex: 0,
-        birthday: '2019-04-04',
-        age: '6月龄',
-        textTime: '2019-10-04',
-        changeTime: '2019-10-09',
-        result: '我家宝宝不在社区，我们下周来进行体检3。'
-      },
-      {
-        name: '袁明轩',
-        sex: 1,
-        birthday: '2019-06-30',
-        age: '3月龄',
-        textTime: '2019-09-30',
-        changeTime: '2019-10-09',
-        result: '我家宝宝不在社区，我们下周来进行体检4。'
-      }],
+      list: [
+        // {
+        //   name: '任慕瑶',
+        //   sex: 0,
+        //   birthday: '2019-04-04',
+        //   age: '6',
+        //   textTime: '2019-10-04',
+        //   changeTime: '2019-10-09',
+        //   result: '我家宝宝不在社区，我们下周来进行体检1。'
+        // }
+      ],
       type: 2  //封装列表页面展示类型，1为明日体检列表，2为改期列表，3为逾期列表
     }
-
-  } 
+  },
+  onLoad() {
+    page = 1;
+    this.setData({
+      ['listData.list']: [],
+    });
+    this.onRequest();
+  },
+  onRequest() {
+    let that = this;
+    http.request({
+      url: "baby/changedatebabyslist",
+      method: 'post',
+      data: JSON.stringify({
+        param: {},
+        page: page,
+        size: config.pageSize,
+      }),
+      success: (res) => {
+        if (res.length == 0) {
+          return
+        }
+        page++;
+        var data = that.data.listData.list;
+        data = data.concat(res);
+        var listIndex = 'listData.list';
+        that.setData({
+          [listIndex]: data,
+        });
+      },
+      fail: function(res) {
+        dd.alert({ content: JSON.stringify(res), buttonText: '好的' });
+      }
+    })
+  },
 })
