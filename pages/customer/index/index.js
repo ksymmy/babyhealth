b43f833a2f51c59e3c8ac70929ead358c19034bc
eileen0817 <1322749014@qq.com>
2019-11-20 12:47:06 +0800

    修改下拉和签到

--- pages/customer/index/index.js
+++ pages/customer/index/index.js
@@ -3,16 +3,19 @@ import { showText } from '/app.js';
 import { config } from '/app.js';
 let http = new HTTP(), page = 1;
 var _my$getSystemInfoSync = my.getSystemInfoSync(), windowHeight = _my$getSystemInfoSync.windowHeight;
-var scrollHeight = windowHeight-110;
+var scrollHeight = windowHeight - 110;
 Page({
   data: {
     toUpper: 'toUpper',
     pageHeight: 1200,//scroll-view触底高度
     scrollHeight: 0,//最小scroll-view高度
-    dataFinish: false,//数据加载完全
-    noDataState: false,//无数据状态
     animationInfo: {},
-
+    topPosition: 0,
+    old: {
+      scrolTop: 0,
+    },
+    oldheight: 0,//标签位置
+    newheight: 0, //新窗口高度
     list: [
       // {
       //   sendDate: '',
@@ -31,16 +34,18 @@ Page({
 
   },
   onLoad(query) {
-    let h = 800 * config.pageSize;
+    let h = 760 * 3;
     this.setData({
       'scrollHeight': scrollHeight,
       'pageHeight': h
     })
     page = 1;
+
     this.setData({
       ['list']: [],
     });
     this.onRequest();
+
     // let that = this;
 
     // // 初始发送日期
@@ -86,19 +91,29 @@ Page({
         size: 3,
       }),
       success: (res) => {
+        console.log(res);
+        res=res.reverse();
         let len = res.length;
+        let h2 = 0;
         if (page == 1) {
-          if (len < config.pageSize) {
-            let h = 800 * len;
-            that.setData({
-              'pageHeight': h
-            })
+          let len1 = 0, len2 = 0;
+          for (let i = 0; i < len; i++) {
+            if (res[i].type = 0) {
+              len1++;
+            } else {
+              len2++;
+            }
           }
+        
+          let h = 760 * len1 + 660 * len2;
+          that.setData({
+            'pageHeight': h
+          })
+
           if (len == 0) {
             that.setData({
               'noDataState': true,
               'loadingState': false
-
             })
             return
           }
@@ -109,7 +124,7 @@ Page({
           })
           return
         }
-        if (len < config.pageSize) {
+        if (len < 3) {
           that.setData({
             'dataFinish': true
           })
@@ -117,16 +132,53 @@ Page({
         page++;
         var data = that.data.list;
         data = res.concat(data);
-        var listIndex = 'list';
         that.setData({
-          [listIndex]: data,
+          'list': data,
           'loadingState': false
         });
+        
+         if (page == 2) {
+        let htop = this.data.pageHeight
+        dd.pageScrollTo({
+          scrollTop: htop
+        })
+        } else {
+          that.data.topPosition = that.data.old.scrolTop
+          that.$nextTick(function() {
+            that.data.topPosition = that.data.oldheight
+          });
+        }
+
+        // setTimeout(function() {
+        //   if (page == 2) {
+        //     let htop = that.data.pageHeight - 1000
+        //       dd.pageScrollTo({
+        //         scrollTop: htop
+        //       })
+        //     // that.setData({
+        //     //   topPosition: htop
+        //     // })
+        //   }
+        // }, 1000)
+
       },
       fail: function(res) {
         dd.alert({ content: JSON.stringify(res), buttonText: '好的' });
       }
     })
+  },
+  onScroll(e) {
+    // let newH = e.detail.scrollHeight
+    // if (this.newheight == 0) {
+    //   this.newheight = newH
+    // } else if (this.newheight != newH) {
+    //   this.oldheight = newH - this.newheight
+    //   this.newheight = newH
+    //   console.log(this.newheight, this.oldheight)
+    // }
+    // this.old.scrolTop = e.detail.scrollTop
+
+    // console.log(e.detail.scrollTop + 'sdsdfs');
   },
   toSignIn(e) {// 签到
     let that = this;
@@ -151,9 +203,13 @@ Page({
     });
   },
   toApplyDate(e) {//去申请改期
-    dd.navigateTo({
-      url: '../apply-changeDate/apply-changeDate?examinationId=' + e.target.dataset.val,
-    });
+
+    // this.setData({
+    //   topPosition: htop
+    // })
+    // dd.navigateTo({
+    //   url: '../apply-changeDate/apply-changeDate?examinationId=' + e.target.dataset.val,
+    // });
   },
   toConfirm(e) {// 确认可以按时体检
     let that = this;
