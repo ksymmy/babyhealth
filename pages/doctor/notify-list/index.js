756e9856532742d2bf621f19f8cdc79527f4a00c
eileen0817 <1322749014@qq.com>
2019-11-21 15:58:55 +0800

    修改明日提醒页面接口数据

--- pages/doctor/notify-list/index.js
+++ pages/doctor/notify-list/index.js
@@ -5,7 +5,7 @@ let http = new HTTP();
 let timeperiod = {}
 var page;
 var _my$getSystemInfoSync = my.getSystemInfoSync(), windowHeight = _my$getSystemInfoSync.windowHeight;
-var scrollHeight = windowHeight-40;
+var scrollHeight = windowHeight - 40;
 Page({
   ...msglist,
   data: {
@@ -19,42 +19,43 @@ Page({
       noDataState: false,//无数据状态
       loadingState: false,//加载状态
       list: [
-      //   {
-      //   babyId: 1,
-      //   name: '任慕瑶',
-      //   sex: 0,
-      //   birthday: '2019-06-30',
-      //   age: '3月龄',
-      //   textTime: '2019-09-30'
-      // },
-      // {
-      //   babyId: 2,
-      //   name: '任慕瑶',
-      //   sex: 0,
-      //   birthday: '2019-04-04',
-      //   age: '6月龄',
-      //   textTime: '2019-10-04'
-      // },
-      // {
-      //   babyId: 3,
-      //   name: '袁明轩',
-      //   sex: 1,
-      //   birthday: '2019-07-04',
-      //   age: '3月龄',
-      //   textTime: '2019-10-04'
-      // },
-      // {
-      //   babyId: 4,
-      //   name: '袁明轩',
-      //   sex: 1,
-      //   birthday: '2019-07-04',
-      //   age: '3月龄',
-      //   textTime: '2019-10-04'
-      // }
+        //   {
+        //   babyId: 1,
+        //   name: '任慕瑶',
+        //   sex: 0,
+        //   birthday: '2019-06-30',
+        //   age: '3月龄',
+        //   textTime: '2019-09-30'
+        // },
+        // {
+        //   babyId: 2,
+        //   name: '任慕瑶',
+        //   sex: 0,
+        //   birthday: '2019-04-04',
+        //   age: '6月龄',
+        //   textTime: '2019-10-04'
+        // },
+        // {
+        //   babyId: 3,
+        //   name: '袁明轩',
+        //   sex: 1,
+        //   birthday: '2019-07-04',
+        //   age: '3月龄',
+        //   textTime: '2019-10-04'
+        // },
+        // {
+        //   babyId: 4,
+        //   name: '袁明轩',
+        //   sex: 1,
+        //   birthday: '2019-07-04',
+        //   age: '3月龄',
+        //   textTime: '2019-10-04'
+        // }
       ],
-      type: 1 //封装列表页面展示类型，1为明日体检列表，2为改期列表，3为逾期列表
-    }
 
+      type: 1 //封装列表页面展示类型，1为明日体检列表，2为改期列表，3为逾期列表
+    },
+    flag: true
   },
   onRequest(page) {
     let that = this;
@@ -69,10 +70,9 @@ Page({
       }),
       success: function(res) {
         let len = res.length;
-
         if (page == 1) {
           if (len < config.pageSize) {
-            let h = 135 * len + 10;
+            let h = 135 * len;
             that.setData({
               'listData.pageHeight': h
             })
@@ -80,45 +80,40 @@ Page({
           if (len == 0) {
             that.setData({
               'listData.noDataState': true,
-              'listData.pageHeight':scrollHeight,
+              'listData.pageHeight': scrollHeight,
               'listData.loadingState': false
+
             })
-            // return
+            return
           }
         } else if (len == 0) {
           that.setData({
             'listData.dataFinish': true,
             'listData.loadingState': false
           })
-          // return
+          return
         }
         if (len < config.pageSize) {
           that.setData({
             'listData.dataFinish': true
           })
         }
-        var newData, result_data, newpagesize, add_size;
-        newData = []
-
-
-        for(var key in res){
-            newData.push(res[key])
-        }
-        if(page===1){
-          add_size=0
-          result_data=newData
-        }else{
-          var oldData = that.data.listData.list;
-          add_size=10
-          result_data = oldData.concat(newData)
-        }
-        newpagesize = that.data.pagesize+add_size
-        page++
+        page++;
+        var data = that.data.listData.list;
+        data = data.concat(res);
+        var listIndex = 'listData.list';
         that.setData({
-          'listData.list': result_data,
-          'pagesize': newpagesize,
+          [listIndex]: data,
           'listData.loadingState': false
-        })
+        });
+      },
+      fail: function(res) {
+        dd.alert({ content: JSON.stringify(res), buttonText: '好的' });
+      },
+      complete: res => {
+        that.setData({
+          'flag': true
+        });
       }
     })
   },
@@ -131,13 +126,18 @@ Page({
       'listData.scrollHeight': scrollHeight,
       'listData.pageHeight': h
     })
-    page = 1
+    page = 1;
+    this.setData({
+      ['listData.list']: [],
+    });
     this.onRequest(page)
   },
   toLower(e) {
 
     let that = this;
-    this.onRequest(page);
+    if (this.data.flag) {
+      this.onRequest(page);
+    }
   },
   //延后一天操作
   handleDelay(e) {
@@ -151,33 +151,31 @@ Page({
     //   },
     // });
     http.request({
-    // url:"baby/delayoneday",
-    url:"baby/delayoneday?examId="+examid,
-    method:'POST',
-    // data:JSON.stringify({
-    //   "examId":examid
-    // }),
-    success:function(res){
-      dd.showToast({
-        content: '已延后',
-        duration:3000,
-        success:function(res){
-        },
-        fail:function(res){
-        },
-        complete:function(res){
-          dd.hideToast()
-        }
-      })
-      page=1
-      that.onRequest(page)
-    },
-    complete:function(res){
-    }
-  })
+      // url:"baby/delayoneday",
+      url: "baby/delayoneday?examId=" + examid,
+      method: 'POST',
+      // data:JSON.stringify({
+      //   "examId":examid
+      // }),
+      success: function(res) {
+        dd.showToast({
+          content: '已延后',
+          duration: 3000,
+          success: function(res) {
+          },
+          fail: function(res) {
+          },
+          complete: function(res) {
+            dd.hideToast()
+          }
+        })
+        page = 1
+        that.onRequest(page)
+      },
+      complete: function(res) {
 
-
-
+      }
+    })
 
   }
 })
